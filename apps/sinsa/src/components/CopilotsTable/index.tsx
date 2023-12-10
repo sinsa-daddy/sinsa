83db555f9034a98d4960615b1fc1254854233032
HeskeyBaozi <hezhiyu233@foxmail.com>
2023-12-10 13:27:10 +0800

    fix: fix pagination

--- apps/sinsa/src/components/CopilotsTable/index.tsx
+++ apps/sinsa/src/components/CopilotsTable/index.tsx
@@ -42,7 +42,7 @@ export const CopilotsTable: React.FC<CopilotsTableProps> = ({
       columns={copilotsColumns}
       params={deps}
       request={request}
-      pagination={{ pageSize: 5 }}
+      pagination={{ pageSize: 5, size: 'default' }}
     />
   );
 };
