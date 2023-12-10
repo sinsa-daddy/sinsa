302ab2470a3cee5843053724fb9f3f480e46a672
HeskeyBaozi <hezhiyu233@foxmail.com>
2023-12-10 14:44:29 +0800

    chore: update avatar

---
+++ apps/sinsa/src/components/UploadForm/index.tsx
@@ -0,0 +1,17 @@
+import { ProForm, ProFormSelect } from '@ant-design/pro-form';
+import { useModel } from '@modern-js/runtime/model';
+import { TermsModel } from '@/models/terms';
+
+export const UploadForm: React.FC = () => {
+  const [{ termsOptions }] = useModel(TermsModel);
+  return (
+    <ProForm>
+      <ProFormSelect
+        name="term"
+        label="荒典期数"
+        options={termsOptions}
+        rules={[{ required: true }]}
+      />
+    </ProForm>
+  );
+};
---
+++ apps/sinsa/src/routes/upload/page.tsx
@@ -0,0 +1,15 @@
+import { PageContainer } from '@ant-design/pro-layout';
+import { UploadForm } from '@/components/UploadForm';
+
+const UploadPage: React.FC = () => {
+  return (
+    <PageContainer
+      title="作业收录（内部版）"
+      content="内部的收录同学可以使用此页面快速上传作业，之后会再提供 Chrome 插件提高效率（比心）"
+    >
+      <UploadForm />
+    </PageContainer>
+  );
+};
+
+export default UploadPage;
