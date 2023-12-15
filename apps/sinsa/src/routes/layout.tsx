f5217e0a3a0a7ee6dd33098d16ae3e5de8126331
HeskeyBaozi <hezhiyu233@foxmail.com>
2023-12-15 12:47:09 +0800

    fix: fix start_url

--- apps/sinsa/src/routes/layout.tsx
+++ apps/sinsa/src/routes/layout.tsx
@@ -1,42 +1,43 @@
-import { Outlet, useLoaderData } from '@modern-js/runtime/router';
-import { ConfigProvider, type ThemeConfig } from 'antd';
-import { useModel } from '@modern-js/runtime/model';
-import { isEmpty } from 'lodash-es';
-import { MyLayout } from '../components/MyLayout';
-import type { LayoutLoaderData } from './layout.data';
-import { TermsModel } from '@/models/terms';
-import { AuroriansModel } from '@/models/aurorians';
-import { useEffect } from 'react';
-import LogoURL from '@/assets/wrench-512.svg';
+import { Outlet, useLoaderData } from "@modern-js/runtime/router";
+import { ConfigProvider, type ThemeConfig } from "antd";
+import { useModel } from "@modern-js/runtime/model";
+import { isEmpty } from "lodash-es";
+import { MyLayout } from "../components/MyLayout";
+import type { LayoutLoaderData } from "./layout.data";
+import { TermsModel } from "@/models/terms";
+import { AuroriansModel } from "@/models/aurorians";
+import { useEffect } from "react";
+import LogoURL from "@/assets/wrench-512.svg";
 
 const theme: ThemeConfig = {
   token: {
-    colorPrimary: 'rgb(220, 89, 80)',
+    colorPrimary: "rgb(220, 89, 80)",
   },
 };
 
-const manifest = {
-  name: '红油扳手作业站',
-  start_url: './index.html',
-  display: 'standalone',
-  description: '您的荒典抄作业小帮手',
-  icons: [
-    {
-      type: 'image/svg+xml',
-      sizes: '48x48 72x72 96x96 128x128 144x144 256x256 512x512',
-      src: LogoURL,
-      purpose: 'any',
-    },
-  ],
-};
-
-const stringManifest = JSON.stringify(manifest);
-
 export default function Layout() {
   useEffect(() => {
-    const blob = new Blob([stringManifest], { type: 'application/json' });
+    const urlBase = `${window.location.protocol}//${window.location.host}`;
+    const manifest = {
+      name: "红油扳手作业站",
+      start_url: `${urlBase}/`,
+      display: "standalone",
+      description: "您的荒典抄作业小帮手",
+      icons: [
+        {
+          type: "image/svg+xml",
+          sizes: "48x48 72x72 96x96 128x128 144x144 256x256 512x512",
+          src: LogoURL,
+          purpose: "any",
+        },
+      ],
+    };
+    const stringManifest = JSON.stringify(manifest);
+    const blob = new Blob([stringManifest], { type: "application/json" });
     const manifestURL = URL.createObjectURL(blob);
-    document.querySelector('#manifest-pwa-placeholder')?.setAttribute('href', manifestURL);
+    document
+      .querySelector("#manifest-pwa-placeholder")
+      ?.setAttribute("href", manifestURL);
   }, []);
 
   const { terms: termsFromRemote, auroriansMap: auroriansMapFromRemote } =
