934e4380c3be782b61301540b632d7c495f98a5b
HeskeyBaozi <hezhiyu233@foxmail.com>
2023-12-15 11:44:55 +0800

    Update workbox-config.js

--- apps/sinsa/workbox-config.js
+++ apps/sinsa/workbox-config.js
@@ -6,12 +6,6 @@ module.exports = {
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   skipWaiting: true,
   clientsClaim: true,
-  additionalManifestEntries: [
-    'https://registry.npmmirror.com/react/18.2.0/files/umd/react.production.min.js',
-    'https://registry.npmmirror.com/dayjs/1.11.10/files/dayjs.min.js',
-    'https://registry.npmmirror.com/antd/5.11.3/files/dist/antd.min.js',
-    'https://registry.npmmirror.com/react-dom/18.2.0/files/umd/react-dom.production.min.js',
-  ],
             runtimeCaching: [
              {
                method: 'GET',
