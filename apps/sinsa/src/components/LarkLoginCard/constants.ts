59e4f3612ddbe57c015fbe915b9dc4b79f43d7c7
HeskeyBaozi <hezhiyu233@foxmail.com>
2023-12-17 09:19:54 +0800

    chore: use prouction

--- apps/sinsa/src/components/LarkLoginCard/constants.ts
+++ apps/sinsa/src/components/LarkLoginCard/constants.ts
@@ -3,4 +3,6 @@ export const UPLOAD_APP_ID = 'cli_a5fd9725777a100b';
 export const LARK_SCOPE = `bitable:app`;
 
 export const LARK_ORIGIN =
-  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
+  process.env.NODE_ENV === 'production'
+    ? 'http://8.134.125.149/api'
+    : 'http://localhost:3000';
