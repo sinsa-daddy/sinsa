f38ac50d9dfd93b263d6a4320221f20c2db53373
HeskeyBaozi <hezhiyu233@foxmail.com>
2023-12-20 01:37:28 +0800

    chore: use lf

--- apps/sinsa/workbox-config.js
+++ apps/sinsa/workbox-config.js
@@ -1,20 +1,20 @@
 module.exports = {
   globDirectory: 'dist/',
   globPatterns: ['**/*.{json,html,css,webp,js,svg}'],
-  globIgnores: ['**\/node_modules\/**\/*', '**\/api\/copilots\/*.json'],
+  globIgnores: ['**/node_modules/**/*', '**/api/copilots/*.json'],
   swDest: 'dist/sw.js',
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   skipWaiting: true,
   clientsClaim: true,
-            runtimeCaching: [
-             {
-               method: 'GET',
-               urlPattern: /\/api\/copilots\/\d+\.json/,
-               handler: 'NetworkFirst',
-               options: {
-                 cacheName: 'Copilots',
-                 networkTimeoutSeconds: 8,
-               },
-             },
-          ],
+  runtimeCaching: [
+    {
+      method: 'GET',
+      urlPattern: /\/api\/copilots\/\d+\.json/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'Copilots',
+        networkTimeoutSeconds: 8,
+      },
+    },
+  ],
 };
