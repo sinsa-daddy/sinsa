8064a3c7f754f766d68972d9962263b87120a38d
HeskeyBaozi <hezhiyu233@foxmail.com>
2024-01-20 00:16:09 +0800

    refactor: chunk for avatar

--- apps/sinsa/modern.config.ts
+++ apps/sinsa/modern.config.ts
@@ -1,6 +1,73 @@
+import { readdirSync } from 'fs';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { appTools, defineConfig } from '@modern-js/app-tools';
+import { first, groupBy, mapValues } from 'lodash-es';
 import { formatOutputPlugin } from './plugins/foramt-output';
 import { fetchDataSourcePlugin } from './plugins/fetch-data-source-plugin';
+
+const AVATARS_PATH = resolve(
+  dirname(fileURLToPath(import.meta.url)),
+  './src/assets/skins',
+);
+const AVATARS_FILENAMES = readdirSync(AVATARS_PATH);
+
+type NameMap = { [key: string]: number };
+
+function splitNames(names: NameMap, numSubsets: number): NameMap[] {
+  // 初始化子集
+  const subsets: NameMap[] = new Array(numSubsets).fill(null).map(() => ({}));
+
+  // 按照数量从大到小的顺序对首字母进行排序
+  const sortedNames = Object.keys(names).sort((a, b) => names[b] - names[a]);
+
+  // 将每个名字添加到当前数量最小的子集中
+  for (const letter of sortedNames) {
+    const minSubset = subsets.reduce(
+      (min, subset) =>
+        Object.keys(subset).length === 0 || sum(subset) < sum(min)
+          ? subset
+          : min,
+      subsets[0],
+    );
+    minSubset[letter] = (minSubset[letter] || 0) + names[letter];
+  }
+
+  return subsets;
+}
+
+// 辅助函数：计算 Map 中值的和
+function sum(map: NameMap): number {
+  return Object.values(map).reduce((total, count) => total + count, 0);
+}
+
+const namesMap: NameMap = mapValues(
+  groupBy(AVATARS_FILENAMES, name => first(name)),
+  array => array.length,
+);
+
+function createCacheGroups(k: number) {
+  const cacheGroup: Record<
+    string,
+    { chunks: 'all'; name: string; test: RegExp }
+  > = {};
+
+  splitNames(namesMap, k).forEach(subset => {
+    const range = Object.keys(subset).join('');
+
+    const name = `asset-avatars-${range}`;
+
+    cacheGroup[name] = {
+      test: new RegExp(
+        `src[\\\\/]assets[\\\\/]skins[\\\\/][${range}].*\\.webp$`,
+      ),
+      name,
+      chunks: 'all',
+    };
+  });
+
+  return cacheGroup;
+}
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig<'rspack'>({
@@ -26,6 +93,11 @@ export default defineConfig<'rspack'>({
   performance: {
     chunkSplit: {
       strategy: 'split-by-experience',
+      override: {
+        cacheGroups: {
+          ...createCacheGroups(10),
+        },
+      },
     },
   },
   output: {
