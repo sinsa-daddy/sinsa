865fe4decd3d5787b7b0bfa96b238329af94cee8
HeskeyBaozi <hezhiyu233@foxmail.com>
2024-02-11 09:52:23 +0800

    fix: sw

--- packages/service-worker/src/index.ts
+++ packages/service-worker/src/index.ts
@@ -1,17 +1,16 @@
-import {
-  precacheAndRoute,
-  addPlugins,
-  cleanupOutdatedCaches,
-} from 'workbox-precaching';
+import { cleanupOutdatedCaches } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { CacheFirst, NetworkFirst } from 'workbox-strategies';
-import { clientsClaim, skipWaiting } from 'workbox-core';
+import {
+  CacheFirst,
+  NetworkFirst,
+  StaleWhileRevalidate,
+} from 'workbox-strategies';
+import { clientsClaim } from 'workbox-core';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { ExpirationPlugin } from 'workbox-expiration';
 
 declare const self: ServiceWorkerGlobalScope;
 
-skipWaiting();
 clientsClaim();
 cleanupOutdatedCaches();
 
@@ -44,31 +43,77 @@ registerRoute(
   }),
 );
 
-addPlugins([
-  {
-    async cacheDidUpdate({ request, cacheName, newResponse }) {
-      if (request.url.includes('api/v2/terms.json')) {
-        const windows = await self.clients.matchAll({
-          type: 'window',
-          includeUncontrolled: true,
-        });
+registerRoute(
+  ({ sameOrigin, request }) => sameOrigin && request.destination === 'script',
+  new StaleWhileRevalidate({
+    cacheName: 'StaticScripts',
+  }),
+);
 
-        const responseData = await newResponse.clone().json();
-        const payloadData = Array.isArray(responseData) ? responseData : [];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function () {
+            // Return true if you want to remove this cache,
+            // but remember that caches are shared across
+            // the whole origin
+            return true;
+          })
+          .map(function (cacheName) {
+            return caches.delete(cacheName);
+          }),
+      );
+    }),
+  );
+  // Skip over the "waiting" lifecycle state, to ensure that our
+  // new service worker is activated immediately, even if there's
+  // another tab open controlled by our older service worker code.
+  self.skipWaiting();
+});
 
-        for (const win of windows) {
-          win.postMessage({
-            type: 'TERMS_UPDATE',
-            payload: { cacheName, updatedURL: request.url, terms: payloadData },
-          });
-        }
-      }
-    },
-  },
-]);
-/**
- * The precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-precacheAndRoute(self.__WB_MANIFEST, {});
+self.addEventListener('activate', () => {
+  // Optional: Get a list of all the current open windows/tabs under
+  // our service worker's control, and force them to reload.
+  // This can "unbreak" any open windows/tabs as soon as the new
+  // service worker activates, rather than users having to manually reload.
+  self.clients
+    .matchAll({
+      type: 'window',
+    })
+    .then(windowClients => {
+      windowClients.forEach(windowClient => {
+        windowClient.navigate(windowClient.url);
+      });
+    });
+});
+
+// addPlugins([
+//   {
+//     async cacheDidUpdate({ request, cacheName, newResponse }) {
+//       if (request.url.includes('api/v2/terms.json')) {
+//         const windows = await self.clients.matchAll({
+//           type: 'window',
+//           includeUncontrolled: true,
+//         });
+
+//         const responseData = await newResponse.clone().json();
+//         const payloadData = Array.isArray(responseData) ? responseData : [];
+
+//         for (const win of windows) {
+//           win.postMessage({
+//             type: 'TERMS_UPDATE',
+//             payload: { cacheName, updatedURL: request.url, terms: payloadData },
+//           });
+//         }
+//       }
+//     },
+//   },
+// ]);
+// /**
+//  * The precacheAndRoute() method efficiently caches and responds to
+//  * requests for URLs in the manifest.
+//  * See https://goo.gl/S9QRab
+//  */
+// precacheAndRoute(self.__WB_MANIFEST, {});
