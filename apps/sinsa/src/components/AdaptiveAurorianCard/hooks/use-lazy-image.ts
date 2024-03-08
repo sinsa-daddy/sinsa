e6001033ef782ffbdf58360b68013cc41786181d
HeskeyBaozi <hezhiyu233@foxmail.com>
2024-03-08 22:04:10 +0800

    feat: update avatar

--- apps/sinsa/src/components/AdaptiveAurorianCard/hooks/use-lazy-image.ts
+++ apps/sinsa/src/components/AdaptiveAurorianCard/hooks/use-lazy-image.ts
@@ -1,11 +1,26 @@
 import { useEffect, useRef } from 'react';
 
-export function useLazyImage(aurorianId?: string) {
+interface LazyImageOptions {
+  type?: 'awaken' | 'skin-1';
+}
+
+const FileNameTypeMap: Record<string, LazyImageOptions | undefined> = {
+  bethel: { type: 'awaken' },
+  luke: { type: 'awaken' },
+  zarya: { type: 'skin-1' },
+};
+
+export function useLazyImage(aurorianId: string | undefined) {
   const containerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const loadImage = async () => {
       if (aurorianId && containerRef.current) {
-        containerRef.current.style.backgroundImage = `url(/assets/images/avatars/${aurorianId}.webp)`;
+        const options = FileNameTypeMap[aurorianId];
+        const filename = options?.type
+          ? `${aurorianId}-${options.type}`
+          : aurorianId;
+
+        containerRef.current.style.backgroundImage = `url(/assets/images/avatars/${filename}.webp)`;
       }
     };
     loadImage();
