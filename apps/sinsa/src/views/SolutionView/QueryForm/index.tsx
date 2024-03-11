956bb8754b042c9a5f912ab8ddc0bb9e020ea6df
HeskeyBaozi <hezhiyu233@foxmail.com>
2024-03-11 21:23:18 +0800

    feat: remove log

--- apps/sinsa/src/views/SolutionView/QueryForm/index.tsx
+++ apps/sinsa/src/views/SolutionView/QueryForm/index.tsx
@@ -138,12 +138,12 @@ export const QueryForm: React.FC<QueryFormProps> = ({ termId, copilots }) => {
           ) : null;
         }}
       </ProFormDependency>
-      <ProFormDependency name={[ensureQueryKey('copilotsDiff')]}>
+      {/* <ProFormDependency name={[ensureQueryKey('copilotsDiff')]}>
         {({ copilotsDiff }: Pick<QueryParamsType, 'copilotsDiff'>) => {
           console.log('copilotsDiff', copilotsDiff);
           return null;
         }}
-      </ProFormDependency>
+      </ProFormDependency> */}
     </ProForm>
   );
 };
