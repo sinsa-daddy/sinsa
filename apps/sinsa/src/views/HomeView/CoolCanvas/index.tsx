ae8160d04f31c408286a4d38a969736c209cc067
HeskeyBaozi <hezhiyu233@foxmail.com>
2024-03-23 10:17:11 +0800

    feat: nice ani

---
+++ apps/sinsa/src/views/HomeView/CoolCanvas/index.tsx
@@ -0,0 +1,118 @@
+/* eslint-disable no-bitwise */
+import React, { useEffect, useRef } from 'react';
+import styles from './styles.module.less';
+
+export const CoolCanvas = React.memo(() => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const convas = canvasRef.current;
+    const ctx = canvasRef.current!.getContext('2d')!;
+
+    if (!(ctx && convas)) {
+      return;
+    }
+
+    const pr = window.devicePixelRatio || 1;
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+    const bandwidth = 180;
+    let q: any[];
+    let r = 0;
+    const twoPi = 2 * Math.PI;
+    const { cos } = Math;
+    const { random } = Math;
+    convas.width = w * pr;
+    convas.height = h * pr;
+    ctx.scale(pr, pr);
+    ctx.globalAlpha = 0.3;
+    const verticals: { x: number; y: number; v_x: number; v_y: number }[] = [];
+    const verticals_side: any[] = [];
+    const fillStyles: string[] = [];
+    let firstTime = !0;
+
+    function i() {
+      if ((ctx.clearRect(0, 0, w, h), firstTime)) {
+        for (
+          q = [
+            { x: 0, y: 0.5 * h + bandwidth, v_x: 0, v_y: 0 },
+            { x: 0, y: 0.5 * h - bandwidth, v_x: 0, v_y: 0 },
+          ],
+            verticals.push(q[0]),
+            verticals.push(q[1]);
+          q[1].x < w + bandwidth;
+
+        ) {
+          firstDraw(q[0], q[1]);
+        }
+      } else {
+        for (let t = 0, e = 1; e < verticals.length - 1; ) {
+          tweak(t, e);
+          t++;
+          e++;
+        }
+      }
+      firstTime = !1;
+      requestAnimationFrame(i);
+    }
+    function go(t: { x: any; y: any; v_x: any; v_y: any }) {
+      random() < 0.5 || (t.v_x += 0.1 * (random() - 0.5));
+      random() < 0.5 || (t.v_y += 0.1 * (random() - 0.499));
+      const e = 0.3;
+      t.v_x = Math.max(Math.min(t.v_x, e), -e);
+      t.v_y = Math.max(Math.min(t.v_y, e), -e);
+      t.x += t.v_x;
+      t.y += t.v_y;
+      (t.x < 0 || t.x > w) && (t.v_x *= -1.01);
+      (t.y < 0 || t.y > h) && (t.v_y *= -1.01);
+    }
+    function tweak(t: number, e: number) {
+      ctx.beginPath();
+      const i = verticals[t];
+      const a = verticals[e];
+      const o = verticals_side[t];
+      const r = fillStyles[t];
+      go(a);
+      ctx.moveTo(i.x, i.y);
+      ctx.lineTo(a.x, a.y);
+      go(o);
+      ctx.lineTo(o.x, o.y);
+      ctx.closePath();
+      ctx.strokeStyle = r;
+      ctx.stroke();
+    }
+    function firstDraw(
+      t: { x: number; y: number },
+      e: { x: number; y: number },
+    ) {
+      ctx.beginPath();
+      ctx.moveTo(t.x, t.y);
+      ctx.lineTo(e.x, e.y);
+      const i = e.x + (2 * random() - 0.25) * bandwidth;
+      const a = y(e.y);
+      ctx.lineTo(i, a);
+      ctx.closePath();
+      r -= twoPi / -50;
+      ctx.strokeStyle = `#${(
+        ((127 * cos(r) + 128) << 16) |
+        ((127 * cos(r + twoPi / 3) + 128) << 8) |
+        (127 * cos(r + (twoPi / 3) * 2) + 128)
+      ).toString(16)}`;
+      ctx.stroke();
+      q[0] = q[1];
+      q[1] = { x: i, y: a, v_x: 0, v_y: 0 };
+      verticals.push(q[1]);
+      fillStyles.push(ctx.strokeStyle);
+      verticals_side.push(q[1]);
+    }
+    function y(t: number): any {
+      const e = t + (2 * random() - 1.1) * bandwidth;
+      return e > h || 0 > e ? y(t) : e;
+    }
+    document.addEventListener('touchmove', function (t) {
+      t.preventDefault();
+    });
+
+    window.requestAnimationFrame(i);
+  }, []);
+  return <canvas className={styles.Cool} ref={canvasRef} />;
+});
---
+++ apps/sinsa/src/views/HomeView/index.tsx
@@ -0,0 +1,100 @@
+import type { DescriptionsProps } from 'antd';
+import {
+  Button,
+  Card,
+  Flex,
+  Image,
+  Tag,
+  Typography,
+  Descriptions,
+  Space,
+} from 'antd';
+import React from 'react';
+import { Link } from '@modern-js/runtime/router';
+import styles from './page.styles.module.less';
+import { HeroSection } from './Hero';
+import { CoolCanvas } from './CoolCanvas';
+import QunURL from '@/assets/docs/qun.webp';
+// import { DarkModeButton } from '@/components/DarkModeButton';
+
+const IS_MAIN =
+  window.location.hostname === 'sinsa-daddy.com' ||
+  window.location.hostname === 'localhost';
+
+const items: DescriptionsProps['items'] = [
+  {
+    label: '首发站点',
+    key: '1st',
+    children: (
+      <Space>
+        <Typography.Link href="https://sinsa-daddy.com" target="_self">
+          https://sinsa-daddy.com
+        </Typography.Link>
+        <Tag color="blue">推荐</Tag>
+      </Space>
+    ),
+  },
+  {
+    label: '备用站点',
+    key: '2nd',
+    children: (
+      <Typography.Link href="https://sinsa-daddy.gitee.io" target="_blank">
+        https://sinsa-daddy.gitee.io
+      </Typography.Link>
+    ),
+  },
+  {
+    label: 'Github',
+    key: 'github',
+    children: (
+      <Typography.Link
+        href="https://github.com/sinsa-daddy/sinsa"
+        target="_blank"
+      >
+        https://github.com/sinsa-daddy/sinsa
+      </Typography.Link>
+    ),
+  },
+];
+
+export const HomeView = React.memo(() => {
+  return (
+    <>
+      <CoolCanvas />
+      <HeroSection />
+      <Flex className={styles.MainBody} vertical wrap="nowrap" gap={12}>
+        <Card>
+          <Descriptions size="small" column={1} items={items} />
+        </Card>
+
+        <Card>
+          <Typography.Paragraph>
+            如果您是我们的作业收录同学，可以从这里跳转到对应收录页面，需要您
+            <Typography.Text strong>
+              有飞书账号且加入我们红油扳手飞书群
+            </Typography.Text>
+          </Typography.Paragraph>
+          <div>
+            {IS_MAIN ? (
+              <Link to={`/upload`}>
+                <Button type="primary">去收录</Button>
+              </Link>
+            ) : (
+              <Link to={`https://sinsa-daddy.com/upload`} target="_self">
+                <Button type="primary">去 sinsa-daddy.com 收录</Button>
+              </Link>
+            )}
+          </div>
+        </Card>
+        <Card>
+          <Typography.Paragraph>
+            网站依然正在完善并且变得更好，我们也仍然十分缺少收录作业数据工。如果您也想参与作业收录工作，可以加入我们
+            QQ 群，我们会提供收录权限。
+          </Typography.Paragraph>
+          <Image width={100} src={QunURL} alt="qun" />
+        </Card>
+        <div></div>
+      </Flex>
+    </>
+  );
+});
