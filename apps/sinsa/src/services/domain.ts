f637b9a7bdb7a054855eb096ec4b5204048c4707
HeskeyBaozi <hezhiyu233@foxmail.com>
2024-05-06 23:55:49 +0800

    feat: domain

--- apps/sinsa/src/services/domain.ts
+++ apps/sinsa/src/services/domain.ts
@@ -5,12 +5,13 @@ export const DOMAIN: DomainType =
   window.location.hostname === 'sinsa-daddy.com' ||
   window.location.hostname === 'sinsa.dad'
     ? 'main'
-    : window.location.hostname === 'sinsa-daddy.gitee.io'
+    : window.location.hostname ===
+      'sinsa-daddy-webify-4dvq4ad5366bb-1255364977.tcloudbaseapp.com'
     ? 'backup'
     : 'localhost';
 
 export const DomainText: Record<DomainType, string> = {
-  backup: '备用站点',
+  backup: '腾讯云备用临时站点（之后可能下线）',
   localhost: '开发站点',
   main: '首发站点',
 };
