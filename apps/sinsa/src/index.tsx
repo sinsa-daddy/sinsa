11e42d3a2b9282fe42f21e8dc583641454a071ec
HeskeyBaozi <hezhiyu233@foxmail.com>
2024-05-28 00:08:40 +0800

    chore: remove arms

--- apps/sinsa/src/index.tsx
+++ apps/sinsa/src/index.tsx
@@ -3,12 +3,6 @@ import '@/plugins/dayjs';
 import { serviceWorker } from '@/services/service-worker';
 import '@icon-park/react/styles/index.less';
 
-import(/* webpackChunkName: "lib-arms */ './plugins/arms').then(
-  ({ initArmsRum }) => {
-    initArmsRum();
-  },
-);
-
 export default async function bootstrap(
   _: React.ComponentType,
   innerBootStrap: () => void,
