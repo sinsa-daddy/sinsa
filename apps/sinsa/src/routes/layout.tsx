60d79e3c7f970fa5ad8b0015b8ea86cf352b95e0
HeskeyBaozi <hezhiyu233@foxmail.com>
2024-06-12 21:07:26 +0800

    fix: 修复跳转

--- apps/sinsa/src/routes/layout.tsx
+++ apps/sinsa/src/routes/layout.tsx
@@ -9,6 +9,7 @@ import { AuroriansModel } from '@/models/aurorians';
 import { GlobalLayout } from '@/views/GlobalLayout';
 import { ServiceWorkerUpdateBanner } from '@/components/ServiceWorkerUpdateBanner';
 import { XFeatureBanner } from '@/components/XFeatureBanner';
+import { redirectToUploadPageWhenGettingCode } from '@/services/feishu-oauth';
 
 const Layout: React.FC = () => {
   const { terms: termsFromRemote, auroriansMap: auroriansMapFromRemote } =
@@ -25,6 +26,8 @@ const Layout: React.FC = () => {
     if (isEmpty(aurorians.auroriansMap)) {
       auroriansActions.setAuroriansMap(auroriansMapFromRemote);
     }
+
+    redirectToUploadPageWhenGettingCode();
   }, []);
 
   return (
